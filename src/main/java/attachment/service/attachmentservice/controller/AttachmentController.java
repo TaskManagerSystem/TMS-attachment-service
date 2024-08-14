e054135ceb2454f1c84310dead7b7210729e8b86
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-14 17:36:45 +0300

    refactor: deleted unused fields

--- src/main/java/attachment/service/attachmentservice/controller/AttachmentController.java
+++ src/main/java/attachment/service/attachmentservice/controller/AttachmentController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
