a483e9e2fb9347a74d9c6f585e1b6dde0f0c31fc
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-08-21 16:59:38 +0300

    refactor/changed kafka config

--- src/main/java/attachment/service/attachmentservice/config/KafkaConsumerConfig.java
+++ src/main/java/attachment/service/attachmentservice/config/KafkaConsumerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 public class KafkaConsumerConfig {
@@ -27,7 +28,7 @@ public class KafkaConsumerConfig {
         configPops.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configPops.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         configPops.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configPops.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configPops.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         configPops.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return new DefaultKafkaConsumerFactory<>(configPops);
     }
