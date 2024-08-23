c8b6ca82dc7a886a67c1cec434e2f78a6af4c4c3
DmytroHadiuchko <153805566+DmytroHadiuchko@users.noreply.github.com>
2024-08-23 13:54:22 +0300

    Changed kafka config (#9)
    * refactor/changed kafka config
    
    * refactor/changed kafka config
    
    * refactor/changed kafka config
    
    ---------
    
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>

--- src/main/java/attachment/service/attachmentservice/config/KafkaProducerConfig.java
+++ src/main/java/attachment/service/attachmentservice/config/KafkaProducerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -17,16 +18,16 @@ public class KafkaProducerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
